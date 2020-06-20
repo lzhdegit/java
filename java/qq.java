2f9dc05687a58fb387680febad0307fd2ee8111a
lzhdegit <1602011125@qq.com>
2020-06-20 14:26:41 +0800

    增加Java代码

---
+++ java/qq.java
@@ -0,0 +1,9 @@
+class  lizi{
+  public static void  main(String[] args){
+
+   System.out.println("xiaozhupeiqi");
+   System.out.println("–°÷Ì≈Â∆Ê");
+
+}
+
+}
---
+++ java/xing.java
@@ -0,0 +1,18 @@
+class  xing{
+  public  static void main(String[] args){
+
+   System.out.println("*");
+   System.out.println("**");
+   System.out.println("***");
+   System.out.println("****");
+   System.out.println("*****");
+   System.out.println("******");
+
+
+
+
+
+}
+
+
+}
